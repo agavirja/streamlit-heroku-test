e0bdfae858dbc89d889b4a718e6040b48cc48238
agavirja <32424558+agavirja@users.noreply.github.com>
2023-08-24 12:46:01 -0500

    crear_app_prueba

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+#streamlit run D:\Dropbox\Empresa\Heroku\prueba_inicial\app.py
+
+st.set_page_config(layout="wide")
+
+st.selectbox("prueba", options=["hola","como","estas"])
